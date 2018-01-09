42a964a25b36ee401ec64a19737428a27ed39c0d
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-01-09 18:35:13 +0100

    Add files via upload

---
+++ WORKINGRADAR/WORKINGRADAR.ino
@@ -0,0 +1,73 @@
+
+#include <Servo.h> 
+
+Servo sweeper;
+
+const int pingPin = 7;
+int pos = 0;
+long duration, cm;
+
+
+void setup() {
+
+  sweeper.attach(6);  
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  // #### Turn to left and measure #### 
+  for(pos = 0; pos <= 180; pos += 3){                                 
+    ping();
+    delay(100);
+    sweeper.write(pos);            
+    delay(300);                     
+  } 
+  
+  // #### Turn to right and measure #### 
+  for(pos = 180; pos>=0; pos-=3){                                
+    ping();
+    delay(100);
+    sweeper.write(pos);            
+    delay(300);            
+  } 
+
+}  
+  
+
+  
+void ping(){
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+
+  // The same pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(pingPin, INPUT);
+  duration = pulseIn(pingPin, HIGH);
+
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
+  
+  //Limit Max Distance to 1 Meter
+  //cm = constrain(cm, 0, 150);  
+  
+
+  Serial.print(cm);
+  Serial.print(",");
+  Serial.print(pos);
+  Serial.println(); 
+ // delay(100);
+  
+}
+
+long microsecondsToCentimeters(long microseconds){
+  return microseconds / 29 / 2;
+}
---
+++ WORKING_RADAR_Graphically_Nice/WORKING_RADAR_Graphically_Nice.pde
@@ -0,0 +1,84 @@
+  
+
+
+
+import processing.serial.*;
+
+Serial myPort;    // The serial port
+
+String inString;  // Input string from serial port
+float pingread;
+float servoread;
+int readings[];
+
+
+float angle;
+float radius;
+
+void setup() {
+  
+  size(900,900);
+  background(0);
+
+  // List all the available serial ports:
+  println(Serial.list());
+  // Open the port you are using at the rate you want:
+  myPort = new Serial(this, Serial.list()[0], 9600);
+  myPort.bufferUntil('\n');
+  
+}
+
+void draw() {
+
+  int middleX = width/2;
+  int middleY = height/2;
+  
+  // background(0);
+  if(servoread > 177){
+    background(0);
+  }
+  if(servoread < 5){
+    background(0);
+  }
+
+ 
+  //text(pingread + "," + servoread, 10,50);
+   
+
+  radius = map(pingread, 0, 150, 0, height * 0.5);
+
+
+  float x = middleX + cos(angle) * height/2;
+  float y = middleY + sin(angle) * height/2;
+  stroke(20);
+  x = middleX + cos(angle) * radius * 2;
+  y = middleY + sin(angle) * radius * 2;
+  stroke(0,255,0);
+  fill(0,255,0);
+  line(middleX, middleY, x, y);
+  noStroke();
+  ellipse(x, y, 7, 7);
+
+
+  
+}
+
+
+
+void serialEvent(Serial p) {
+  
+   inString =  p.readStringUntil('\n');
+  
+   inString = trim(inString);
+   readings = int(split(inString, ','));
+   println("Converted " + readings[0] + "," + readings[1]);
+   println("instring " + inString);
+   pingread = (readings[0]);
+   angle = (readings[1] * 0.018);
+   servoread = readings[1];
+   
+
+   
+}
+
+
